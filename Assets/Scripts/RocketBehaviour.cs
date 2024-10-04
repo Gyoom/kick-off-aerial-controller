e6fcbcbef7e05e4b18f7aef292c9d1a47e87025e
PIETTE Guillaume <g.piette@rubika-edu.com>
2024-10-04 12:58:44 +0200

    add new design rocket

--- Assets/Scripts/RocketBehaviour.cs
+++ Assets/Scripts/RocketBehaviour.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class RocketBehaviour : MonoBehaviour
 {
     private GameObject airplane;
-    private float speed;  
+    private float speed;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         airplane = GameObject.FindGameObjectWithTag("airplane");
         speed = airplane.GetComponent<AirplaneController>().config.flySpeed * 1.3f;
+        rb = gameObject.GetComponent<Rigidbody>();
 
         transform.LookAt(airplane.transform);
         StartCoroutine(SendHoming());
@@ -20,11 +22,14 @@ public class RocketBehaviour : MonoBehaviour
     // Update is called once per frame
     public IEnumerator SendHoming()
     {
-        while(Vector3.Distance(airplane.transform.position, transform.position)  > 0.3f)
+        while(airplane != null && Vector3.Distance(airplane.transform.position, transform.position)  > 0.3f)
         {
             Vector3 direction = airplane.transform.position - transform.position;
             direction = direction.normalized;
-            transform.position += direction * speed * Time.deltaTime;
+
+            //transform.position += direction * speed * Time.deltaTime;
+            rb.velocity = direction * speed;
+
             yield return null;
         }
         Destroy(gameObject);
